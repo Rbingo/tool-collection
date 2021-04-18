67afd3e7675c152d8531f5913aeb4024b7502ccc
十二月 <877491187@qq.com>
2021-04-18 23:37:19 +0800

    d

--- utu-identify/ad.py
+++ utu-identify/ad.py
@@ -7,19 +7,21 @@ import date
 # print(date.time_extract(text_1))
 # exit()
 
-# location_str = ["【起点】宜丰、上高"]
+location_str = ["【起点】塘厦"]
 import addressparser
-# df = addressparser.transform(location_str)
-# print(df)
-# exit(0)
+df = addressparser.transform(location_str)
+print(df)
+exit(0)
+
+import pymysql
 
 
-str_address ='''（类型） 人找车
-（时间） 4月15号16号
-（起点）深圳，东莞
-（终点） 上高
-（人数）1个位
-电话13632695311'''
+str_address ='''【类型】车找人 
+【时间】4月24号早上
+【起点】塘厦
+【终点】上高
+【座位】4位子
+【电话】13927447458'''
 strs = str_address.splitlines(True)
 # print(strs)
 arr = {}
@@ -32,7 +34,7 @@ for x in strs:
 		pass
 	if '时间' in x:
 		d = date.time_extract(x)
-		arr['time'] = d[0]
+		arr['start_time'] = d[0]
 		pass
 	if '起点' in x:
 		df = addressparser.transform([x])
@@ -80,3 +82,30 @@ for x in strs:
 	pass
 
 print(arr)
+arr['name'] = 'bingo'
+
+cols = ", ".join('`{}`'.format(k) for k in arr.keys())
+print(cols)  # '`name`, `age`'
+
+val_cols = ', '.join('%({})s'.format(k) for k in arr.keys())
+print(val_cols)  # '%(name)s, %(age)s'
+
+sql = "insert into py(%s) values(%s)"
+res_sql = sql % (cols, val_cols)
+print(res_sql)
+# exit(1)
+
+db = pymysql.connect()
+# 使用 cursor() 方法创建一个游标对象 cursor
+cursor = db.cursor()
+# 使用 execute()  方法执行 SQL 查询 
+# ctime=time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+cursor.execute('set profiling = 1')
+try:
+    cursor.execute(res_sql, arr)
+except Exception:
+    cursor.execute('show profiles')
+    for row in cursor:
+        print(row)        
+cursor.execute('set profiling = 0')
+db.commit()
